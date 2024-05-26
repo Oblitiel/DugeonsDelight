8e4262148f554e83c1ddd22ce4f3267de9f7165f
Oblitiel <gabrinator99@gmail.com>
2024-05-26 16:20:15 +0200

    Commit Inicial

--- src/main/java/net/oblitiel/delightindungeon/DelightInDungeon.java
+++ src/main/java/net/oblitiel/delightindungeon/DelightInDungeon.java
@@ -18,7 +18,7 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(DelightInDungeon.MOD_ID)
 public class DelightInDungeon {
-    // Define mod id in a common place for everything to reference
+    // Define mod id in a common place for everything to reference cambio de prueba
     public static final String MOD_ID = "delightindungeon";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
